e1de0cae276c63e58335ad267adf69cc857fd04d
Beyazıt UYSAL <66223190+masanbasa3k@users.noreply.github.com>
2024-01-27 03:09:51 +0300

    fix the missing parts
    date and index

--- take_data.py
+++ take_data.py
@@ -5,18 +5,21 @@ import pandas as pd
 symbols = ['TRY=X', 'USDT=X']
 
 # Data retrieval range and interval
-start_date = '2023-01-01'
-end_date = '2023-02-01'
+start_date = '2020-01-01'
+end_date = '2024-01-26'
 interval = '1d'  # Daily basis
 
 # Retrieve data
 data = yf.download(symbols, start=start_date, end=end_date, interval=interval)
+# data = pd.read_csv("tr_usd_exchance/exchange_rates_data.csv")
 
 df = pd.DataFrame(data)
 
-df = df.iloc[:, :2]
+df = df.iloc[:, :1]
 
 # Save to CSV file
-df.to_csv('tr_usd_exchance/exchange_rates_data.csv', index=False)
+df.to_csv('tr_usd_exchance/exchange_rates_data.csv', index=True)
+
+
 
 
